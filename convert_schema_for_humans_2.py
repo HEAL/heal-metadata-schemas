fbc43c64b33de905b3857ef3b21f3de432ea0efe
Andrea Tentner <tentner-andrea@norc.org>
2022-03-18 13:42:45 -0500

    add script to convert json to md

---
+++ convert_schema_for_humans_2.py
@@ -0,0 +1,22 @@
+
+import sys
+import json
+import jsonschema2md
+
+input_json = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema.json"
+output_md = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema-for-humans.md"
+
+parser = jsonschema2md.Parser(
+    examples_as_yaml=False,
+    show_examples="all",
+    )
+
+with open(input_json,"r") as json_file:
+    md_lines = parser.parse_schema(json.load(json_file))
+
+original_stdout = sys.stdout # save ref to original stdout of print
+
+with open(output_md,"w") as f:
+    sys.stdout = f # change stdout to output md file we created
+    print(''.join(md_lines))
+    sys.stdout = original_stdout # reset stdout to original value
