fadd86fd60f00166ec45e15c3665dae4dc88d389
Andrea Tentner <tentner-andrea@norc.org>
2022-03-22 10:03:47 -0500

    modify the script to convert json to md to make it easy to convert either clean json or cedar json file

--- convert_schema_for_humans_2.py
+++ convert_schema_for_humans_2.py
@@ -1,12 +1,32 @@
 
+file_to_convert = "clean json"
+
 # https://github.com/RalfG/jsonschema2md
 
 import sys
 import json
 import jsonschema2md
 
-input_json = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema.json"
-output_md = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema-for-humans.md"
+input_clean_json = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema.json"
+output_clean_md = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema-for-humans.md"
+
+input_cedar_json = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema-cedar-template.json"
+output_cedar_md = r"C:\Users\tentner-andrea\project_repositories\heal-metadata-schemas\study-metadata-schema-cedar-template-for-humans.md"
+
+
+
+
+if file_to_convert == "clean json":
+    input_json = input_clean_json
+    output_md = output_clean_md
+elif file_to_convert == "cedar json":
+    input_json = input_cedar_json
+    output_md = output_cedar_md
+else: 
+    raise ValueError("file_to_convert must be one of clean json or cedar json")
+
+
+
 
 parser = jsonschema2md.Parser(
     examples_as_yaml=False,
