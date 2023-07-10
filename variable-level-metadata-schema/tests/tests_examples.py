552a1e710222dc9bf45e320e24b6343029629d12
Michael Kranz <mbkranz@gmail.com>
2023-07-10 12:36:02 -0500

    Added invalid tests

--- variable-level-metadata-schema/tests/tests_examples.py
+++ variable-level-metadata-schema/tests/tests_examples.py
@@ -32,16 +32,16 @@ def validate_against_jsonschema(json_object,schema):
 # frictionless schemas --> if csv file
 # json schemas --> if json file
 
-def test_validity_csv_data_dictionaries():
-    csvs = Path(VLMD_EXAMPLE_PATH).glob("*/*.csv")
+def test_valid_csv_data_dictionaries():
+    csvs = Path(VLMD_EXAMPLE_PATH).glob("valid/*.csv")
     csvreports = []
     for filepath in csvs:
         resource = frictionless.Resource(path=filepath,schema=CSV_FRICTIONLESS_SCHEMA_PATH)
         report = resource.validate()
         assert report["valid"],report.to_summary()
 
-def test_validity_json_data_dictionaries():
-    jsons = Path(VLMD_EXAMPLE_PATH).glob("*/*.json")
+def test_valid_json_data_dictionaries():
+    jsons = Path(VLMD_EXAMPLE_PATH).glob("valid/*.json")
     jsonreports = []
     for filepath in jsons:
         json_object = json.loads(filepath.read_text())
@@ -49,7 +49,25 @@ def test_validity_json_data_dictionaries():
         
         # report to etl to pretty print
         report_summary = str(etl.fromdicts(report["errors"]).totext())
+
         assert report["valid"],f"# invalid: {str(filepath)}\n\n{report_summary}"
 
-test_validity_csv_data_dictionaries()
-test_validity_json_data_dictionaries()
+
+def test_invalid_csv_data_dictionaries():
+    csvs = Path(VLMD_EXAMPLE_PATH).glob("invalid/*.csv")
+    csvreports = []
+    for filepath in csvs:
+        resource = frictionless.Resource(path=filepath,schema=CSV_FRICTIONLESS_SCHEMA_PATH)
+        report = resource.validate()
+        assert not report["valid"],f"{str(filepath)} should be an example of an INVALID file but is valid." 
+
+def test_invalid_json_data_dictionaries():
+    jsons = Path(VLMD_EXAMPLE_PATH).glob("invalid/*.json")
+    jsonreports = []
+    for filepath in jsons:
+        json_object = json.loads(filepath.read_text())
+        report = validate_against_jsonschema(json_object, schema=JSON_SCHEMA_PATH)
+        assert not report["valid"],f"{str(filepath)} should be an example of an INVALID file but is valid." 
+ 
+
+test_invalid_csv_data_dictionaries()
