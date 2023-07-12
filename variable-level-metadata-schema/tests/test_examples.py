5bff946508e1a5fd50b0b2f1429c9a72d8e00b2a
Michael Kranz <mbkranz@gmail.com>
2023-07-12 13:05:08 -0500

    test2

--- variable-level-metadata-schema/tests/test_examples.py
+++ variable-level-metadata-schema/tests/test_examples.py
@@ -40,24 +40,24 @@ def validate_against_jsonschema(json_object,schema):
 def test_valid_csv_data_dictionaries():
     csvs = Path(VLMD_EXAMPLE_PATH).glob("valid/*.csv")
     csvreports = []
+    print("Testing:")
+    print(str(filepath))
     for filepath in csvs:
         resource = frictionless.Resource(path=str(filepath),schema=csv_frictionless_schema_object)
         report = resource.validate()
-        print("Testing:")
-        print(str(filepath))
+
         assert report["valid"],f"# this example is invalid but is intended to be valid:\n\n {report.to_summary()}"
 
 def test_valid_json_data_dictionaries():
     jsons = Path(VLMD_EXAMPLE_PATH).glob("valid/*.json")
     jsonreports = []
     for filepath in jsons:
-        json_object = json.loads(filepath.read_text())
-        report = validate_against_jsonschema(json_object, schema=json_schema_object)
-        
-        # report to etl to pretty print
-        report_summary = str(etl.fromdicts(report["errors"]))
         print("Testing:")
         print(str(filepath))
+        json_object = json.loads(filepath.read_text())
+        report = validate_against_jsonschema(json_object, schema=json_schema_object)
+        # report to etl to pretty print
+        report_summary = str(etl.fromdicts(report["errors"]))
         assert report["valid"],f"# this example is invalid but is intended to be valid:\n\n {str(filepath)}\n\n{report_summary}"
 
 
